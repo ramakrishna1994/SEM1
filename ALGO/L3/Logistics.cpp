2e55036fc5b2cf9c91e650ef37bb07d66e12ab8b
saradhi <ramakrishnasaradhi@gmail.com>
2017-11-16 21:04:19 +0530

    ALGO HASH CODE DONE

--- ALGO/L3/Logistics.cpp
+++ ALGO/L3/Logistics.cpp
@@ -6,11 +6,11 @@
 #include <set>
 #include <cmath>
 #include <string>
+#include <limits>
 #define MAX 32000;
 
 using namespace std;
 
-char fileName[] = "sampleinput.in";
 
 int noOfRowsInMap = 0;
 int noOfColumnsInMap = 0;
@@ -35,17 +35,15 @@ bool isCustomerOrderCompleted[10001] = {false};
 int dronesLocation[10001][3];
 int stockInDrone[10001][10001];
 int currentDroneWeight[10001] = {0};
-
-int calculateDistance(int x1,int y1,int x2,int y2)
-{
-	int roundedOfDistance = round(sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
-	return roundedOfDistance;
-}
-
+int distanceTravelledByDrone[10001] = {0};
+int temporaryDistanceCheckForDrone[10001] = {0};
 
 void readInputsFromFile()
 {
-	ifstream infile("sampleinput.in");
+	//ifstream infile("mother_of_all_warehouses.in");
+	//ifstream infile("busy_day.in");
+	ifstream infile("redundancy.in");
+	//ifstream infile("sampleinput.in");
 	string line;
 	int lineNo=1;
 	int count1 = 1;
@@ -154,6 +152,13 @@ void readInputsFromFile()
 	}
 }
 
+int calculateDistance(int x1,int y1,int x2,int y2)
+{
+	int roundedOfDistance = round(sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
+	return roundedOfDistance;
+}
+
+
 void printData()
 {
 	cout << noOfRowsInMap << " " << noOfColumnsInMap << " " << noOfDrones << " " << noOfMaxTurns << " " << maxPayloadDroneCanCarry << endl;
@@ -183,6 +188,25 @@ void printData()
 	}
 }
 
+int isWarehouseContainingSatisfyingMinimumCustomerOrder(int customerOrderId, int warehouseId)
+{
+	int remainingRequiredForCustomer = 0;
+	int totalProductsSatisfiedByWarehouse = 0;
+	for(int i=1;i<=noOfProductTypes;i++)
+	{
+		remainingRequiredForCustomer = quantityOrderedByCustomer[customerOrderId][i] - stockInCustomerLocation[customerOrderId][i];
+		if(remainingRequiredForCustomer > 0)
+		{
+			if(stockInWareHouses[warehouseId][i] > 0)
+				totalProductsSatisfiedByWarehouse ++;
+		}
+	}
+	if(totalProductsSatisfiedByWarehouse > 0)
+		return 1;
+	else
+		return 0;
+}
+
 int findWarehouseCloseToSpecificCustomer(int customerOrderId)
 {
 	int min = MAX;
@@ -195,7 +219,7 @@ int findWarehouseCloseToSpecificCustomer(int customerOrderId)
 		x2 = wareHousesLocation[warehouseId][1];
 		y2 = wareHousesLocation[warehouseId][2];
 		distance = calculateDistance(x1,y1,x2,y2);
-		if(min > distance)
+		if(min > distance && isWarehouseContainingSatisfyingMinimumCustomerOrder(customerOrderId,warehouseId))
 		{
 			min = distance;
 			selectedWareHouseId = warehouseId;
@@ -216,7 +240,7 @@ int findDroneCloseToSpecificWarehouse(int warehouseId)
 		x2 = dronesLocation[droneId][1];
 		y2 = dronesLocation[droneId][2];
 		distance = calculateDistance(x1,y1,x2,y2);
-		if(min > distance)
+		if(min > distance && temporaryDistanceCheckForDrone[droneId]!=1)
 		{
 			min = distance;
 			selectedDroneId = droneId;
@@ -225,43 +249,223 @@ int findDroneCloseToSpecificWarehouse(int warehouseId)
 	return selectedDroneId;
 }
 
+int isWeightOfDroneExceedingMaxPayload(int droneId)
+{
+	int totalWeight = 0;
+	for(int i=1;i<=noOfProductTypes;i++)
+	{
+		totalWeight += stockInDrone[droneId][i] * productTypeWeights[i];
+	}
+	cout << "total weight in drone : " << droneId <<" is "<< totalWeight << endl;
+	if(totalWeight >= maxPayloadDroneCanCarry)
+		return 1;
+	else
+		return 0;
+}
 
-void serviceCustomerOrder(int customerOrderId,int wareHouseId,int droneId)
+void loadProductsFromWarehouseIntoDrone(int customerOrderId,int wareHouseId,int droneId)
 {
 	int remainingRequiredForCustomer = 0;
 	for(int i=1;i<=noOfProductTypes;i++)
 	{
-		remainingRequiredForCustomer = stockInCustomerLocation[customerOrderId][i] - stockInDrone[droneId][i];
-		if(remainingRequiredForCustomer <= stockInWareHouses[wareHouseId][i])
+		remainingRequiredForCustomer = quantityOrderedByCustomer[customerOrderId][i] - stockInCustomerLocation[customerOrderId][i];
+
+		if(remainingRequiredForCustomer > 0)
 		{
-			stockInDrone[droneId][i] += remainingRequiredForCustomer;
+			cout << "remaining for product type " << i << " is "<< remainingRequiredForCustomer << endl;
+			int remainingRequiredInDroneToSatisfyCustomerOrder = remainingRequiredForCustomer - stockInDrone[droneId][i];
+			if(remainingRequiredInDroneToSatisfyCustomerOrder > 0)
+			{
+				if(remainingRequiredInDroneToSatisfyCustomerOrder <= stockInWareHouses[wareHouseId][i])
+				{
+					cout << "remaining required in drone to satisfy customer for product type " << i << " is "<< remainingRequiredInDroneToSatisfyCustomerOrder << endl;
+					cout << "Stock in warehouse of product type " << i << " is " << stockInWareHouses[wareHouseId][i] << endl;
+					stockInDrone[droneId][i] += remainingRequiredInDroneToSatisfyCustomerOrder;
+					stockInWareHouses[wareHouseId][i] -= remainingRequiredInDroneToSatisfyCustomerOrder;
+					if(isWeightOfDroneExceedingMaxPayload(droneId))
+					{
+						cout << "weight exceeded for drone : " << droneId << " so, removing product type : " << i << endl;
+						stockInDrone[droneId][i] -= remainingRequiredInDroneToSatisfyCustomerOrder;
+						stockInWareHouses[wareHouseId][i] += remainingRequiredInDroneToSatisfyCustomerOrder;
+					}
+
+				}
+				else
+				{
+					int temp = stockInWareHouses[wareHouseId][i];
+					cout << "remaining required in drone to satisfy customer for product type " << i << " is "<< remainingRequiredInDroneToSatisfyCustomerOrder << endl;
+					cout << "Stock in warehouse of product type " << i << " is " << stockInWareHouses[wareHouseId][i] << endl;
+					stockInDrone[droneId][i] += temp;
+					stockInWareHouses[wareHouseId][i] = 0;
+					if(isWeightOfDroneExceedingMaxPayload(droneId))
+					{
+						cout << "weight exceeded for drone : " << droneId << " so, removing product type : " << i << endl;
+						stockInDrone[droneId][i] -= temp;
+						stockInWareHouses[wareHouseId][i] += temp;
+					}
+				}
+			}
 
 		}
-		else
-		{
-			stockInDrone[droneId][i] += stockInWareHouses[wareHouseId][i];
-		}
+
 	}
 }
 
+void checkForCustomerOrderCompletionAndSetTheFlag(int customerOrderId)
+{
+	int noOfProductTypesSatisfied = 0;
+	for(int i=1;i<=noOfProductTypes;i++)
+	{
+		if(stockInCustomerLocation[customerOrderId][i] == quantityOrderedByCustomer[customerOrderId][i])
+			noOfProductTypesSatisfied ++;
+	}
+	if(noOfProductTypesSatisfied == noOfProductTypes)
+		{
+			isCustomerOrderCompleted[customerOrderId] = 1;
+			cout << "customer order completed" << endl;
+		}
+}
+
+void updateDroneLocation(int customerOrderId, int droneId)
+{
+	dronesLocation[droneId][1] = customerOrdersLocation[customerOrderId][1];
+	dronesLocation[droneId][2] = customerOrdersLocation[customerOrderId][2];
+	cout << "drone : " << droneId << " location updated to  customer : " << customerOrderId << endl;
+}
+
+
+void updateDistanceTravelledByDrone(int customerOrderId, int warehouseId,int droneId)
+{
+	int x1,y1,x2,y2;
+	x1 = customerOrdersLocation[customerOrderId][1];
+	y1 = customerOrdersLocation[customerOrderId][2];
+	x2 = wareHousesLocation[warehouseId][1];
+	y2 = wareHousesLocation[warehouseId][2];
+	int distanceBetweenCustomerAndWarehouse = calculateDistance(x1,y1,x2,y2);
+	x1 = dronesLocation[droneId][1];
+	y1 = dronesLocation[droneId][2];
+	x2 = wareHousesLocation[warehouseId][1];
+	y2 = wareHousesLocation[warehouseId][2];
+	int distanceBetweenDroneAndWarehouse = calculateDistance(x1,y1,x2,y2);
+	distanceTravelledByDrone[droneId] += distanceBetweenCustomerAndWarehouse + distanceBetweenDroneAndWarehouse;
+	cout << "Updating Distance Travelled by drone : " << droneId << endl;
+}
+
+void deliverProductsToCustomer(int customerOrderId,int selectedDroneId)
+{
+	int remainingRequiredForCustomer = 0;
+	for(int i=1;i<=noOfProductTypes;i++)
+	{
+		//remainingRequiredForCustomer = quantityOrderedByCustomer[customerOrderId][i] - stockInCustomerLocation[customerOrderId][i];
+		if(stockInDrone[selectedDroneId][i] > 0)
+		{
+			cout << "delivering product type " << i << " is " << stockInDrone[selectedDroneId][i] << endl;
+			stockInCustomerLocation[customerOrderId][i] += stockInDrone[selectedDroneId][i];
+			stockInDrone[selectedDroneId][i] = 0;
+		}
+	}
+	checkForCustomerOrderCompletionAndSetTheFlag(customerOrderId);
+	updateDroneLocation(customerOrderId,selectedDroneId);
+}
+
+int willDroneDistanceBeExhaustedBeforeItCanComplete(int customerOrderId,int warehouseId, int droneId)
+{
+	int x1,y1,x2,y2;
+	x1 = customerOrdersLocation[customerOrderId][1];
+	y1 = customerOrdersLocation[customerOrderId][2];
+	x2 = wareHousesLocation[warehouseId][1];
+	y2 = wareHousesLocation[warehouseId][2];
+	int distanceBetweenCustomerAndWarehouse = calculateDistance(x1,y1,x2,y2);
+	x1 = dronesLocation[droneId][1];
+	y1 = dronesLocation[droneId][2];
+	x2 = wareHousesLocation[warehouseId][1];
+	y2 = wareHousesLocation[warehouseId][2];
+	int distanceBetweenDroneAndWarehouse = calculateDistance(x1,y1,x2,y2);
+	int newDistance = distanceTravelledByDrone[droneId] + distanceBetweenCustomerAndWarehouse + distanceBetweenDroneAndWarehouse;
+	if(newDistance > noOfMaxTurns)
+	{
+		temporaryDistanceCheckForDrone[droneId] = 1;
+		return 1;
+	}
+	else
+		return 0;
+
+}
+
+void resetTemporaryDistanceCheckForDrone()
+{
+	for(int i=1;i<=noOfDrones;i++)
+		temporaryDistanceCheckForDrone[i] = 0;
+}
+
+int areAllDroneDistancesExhausted()
+{
+	int count = 0;
+	for(int i=1;i<=noOfDrones;i++)
+		if(temporaryDistanceCheckForDrone[i] == 1)
+			count++;
+	if(count == noOfDrones)
+		return 1;
+	else
+		return 0;
+}
 void startAlgo()
 {
 	for(int customerOrderId=1;customerOrderId<=noOfCustomerOrders;customerOrderId++)
 	{
-		int warehouseIDCloseToCustomer = findWarehouseCloseToSpecificCustomer(customerOrderId);
-		cout << "WareHouse close to customer:" << customerOrderId << " is warehouse:" << warehouseIDCloseToCustomer << endl;
-		int droneCloseToWarehouse = findDroneCloseToSpecificWarehouse(warehouseIDCloseToCustomer);
-		cout << "Drone Close to Warehouse:" << warehouseIDCloseToCustomer << " is drone:" << droneCloseToWarehouse << endl;
-		serviceCustomerOrder(customerOrderId,warehouseIDCloseToCustomer,droneCloseToWarehouse);
-
+		cout << "=============================running for customer: " << customerOrderId << "===============================================" << endl;
+		resetTemporaryDistanceCheckForDrone();
+		while(1){
+			int warehouseIDCloseToCustomer = findWarehouseCloseToSpecificCustomer(customerOrderId);
+			cout << "WareHouse close to customer:" << customerOrderId << " is warehouse:" << warehouseIDCloseToCustomer << endl;
+			int droneCloseToWarehouse = findDroneCloseToSpecificWarehouse(warehouseIDCloseToCustomer);
+			cout << "Drone Close to Warehouse:" << warehouseIDCloseToCustomer << " is drone:" << droneCloseToWarehouse << endl;
+			if(!willDroneDistanceBeExhaustedBeforeItCanComplete(customerOrderId,warehouseIDCloseToCustomer,droneCloseToWarehouse))
+			{
+				loadProductsFromWarehouseIntoDrone(customerOrderId,warehouseIDCloseToCustomer,droneCloseToWarehouse);
+				deliverProductsToCustomer(customerOrderId,droneCloseToWarehouse);
+				updateDistanceTravelledByDrone(customerOrderId,warehouseIDCloseToCustomer,droneCloseToWarehouse);
+			}
+			else
+				cout << "Drone : " << droneCloseToWarehouse << " distance will be exhausted before it can complete . So selecting new drone" << endl;
+			if(isCustomerOrderCompleted[customerOrderId] == 1)
+				break;
+			if(areAllDroneDistancesExhausted())
+			{
+				cout << "All Drones distances are exhaused " << endl;
+				cout << "Customer order cannot be completed" << endl;
+				break;
+			}
+			cout << "----------" << endl;
+		}
 	}
 }
 
+int noOfCustomerOrdersSatisfied()
+{
+	int count = 0;
+	for(int i=1;i<=noOfCustomerOrders;i++)
+		if(isCustomerOrderCompleted[i] == 1)
+			count++;
+	return count;
+}
+
+void printDistancesOfDrones()
+{
+	for(int i=1;i<=noOfDrones;i++)
+		cout << " Distance of drone " << i << " is " << distanceTravelledByDrone[i] << endl;
+}
 
 int main()
 {
+	/*int imin = std::numeric_limits<int>::min(); // minimum value
+	int imax = std::numeric_limits<int>::max();
+	cout << imin << "\t" << imax << endl;*/
 	readInputsFromFile();
 	//printData();
 	startAlgo();
+	cout << "==============================================================================" << endl;
+	cout << "No of customer Orders satisfied are " << noOfCustomerOrdersSatisfied() << " out of total " << noOfCustomerOrders << endl;
+	printDistancesOfDrones();
 
 }
